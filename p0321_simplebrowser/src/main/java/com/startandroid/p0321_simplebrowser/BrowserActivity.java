a57978fc438fa1836a7cd35ce08cca5a8c6cfba1
pavel081215 <pavelzag12@gmail.com>
2017-09-02 20:28:42 +0300

    02/09/2017 23.27

---
+++ p0321_simplebrowser/src/main/java/com/startandroid/p0321_simplebrowser/BrowserActivity.java
@@ -0,0 +1,21 @@
+package com.startandroid.p0321_simplebrowser;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class BrowserActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browser);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        Uri data = getIntent().getData();
+        webView.loadUrl(data.toString());
+    }
+}
